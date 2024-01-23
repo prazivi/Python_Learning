62938d8097e7836c232a63fa23bfe8770bd0c40a
Pranav Gupta <kpranav.in@gmail.com>
2024-01-23 15:27:10 +0530

    dict dataType

---
+++ 01_Basics/dict.py
@@ -0,0 +1,27 @@
+"""
+If we want to represent a group of values as key-value pairs then we should go for dict data type.
+
+In javascript this is called object
+
+Note: dict is mutable and the order wont be preserved.
+
+In general we can use bytes and bytearray data types to represent binary information like images,video files etc
+
+In Python there is no char data type. Hence we can represent char values also by using str type.
+"""
+
+d = {101:'pranav',102:'alok',103:'pranav'}
+
+# print(d[101])  #pranav
+
+d[101] = 'prabhakar'
+print(d)
+a = {}
+a['c']= 'cat'
+a['b'] = 'ball'
+print(a)
+
+
+
+
+
