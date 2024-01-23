021564bda0da8956486f6eeda28e022775d1a964
Pranav Gupta <kpranav.in@gmail.com>
2024-01-23 12:23:26 +0530

    hello

--- 01_Basics/hello.py
+++ 01_Basics/hello.py
@@ -1,1 +1,32 @@
-print(''' This is " character''');
+# print(''' This is " character''');
+s1="""durga 
+         soft"""
+# print(s1)
+
+
+
+s2 ="Pranav"
+# print(s2[0])
+# print(s2[1:40])#ranav
+# print(s2[1:])#ranav
+
+s3 = "a"
+s4= "Pranav"
+print(type(s3))#<class 'str'>
+print(type(s2))#<class 'str'>
+print(s2)
+print(s4)
+
+"""
+In Python the following data types are considered as Fundamental Data types
+* int 
+* float 
+* complex 
+* bool 
+* str
+"""
+
+
+
+
+
